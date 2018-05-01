95e2b7d8cf156e6e6a0dea6e2fa3821691497eeb
BankNatthapon <natthapon.sirinon@gmail.com>
2018-05-02 02:31:44 +0700

    initial commit with staging branch

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <Text>Hello App Center</Text>
+        <Text>Test to Release</Text>
       </View>
     );
   }
